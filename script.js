7ea2e1a715a183ae04837e3067f062483dcc106b
Ritusree Chanda <85223327+Ritusree05@users.noreply.github.com>
2025-03-05 18:37:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function()
         let date = document.getElementById('data1').value;
         let month = document.getElementById('data2').value;
         let year = document.getElementById('data3').value;
-        console.log(date, month, year);
 
         let today = new Date();
         let curDay = today.getDate();
@@ -35,25 +34,18 @@ document.addEventListener("DOMContentLoaded", function()
 
         if (curDay < date)
         {
-            // ageYear = curYear - year - 1;
             ageDay = 30 - (date - curDay);
         }
 
         else if (curDay > date)
         {
-            // ageYear = curYear - year;
             ageDay = curDay - date;
         }
 
         else
         {
-            // ageYear = curYear - year;
             ageDay = 0;
         }
-
-        // let box = document.getElementById('ageBox');
-        // box[0].innerText = ;
-        // box[1].innerText = 
 
         let res_year = document.getElementById('years');
         let res_else = document.getElementById('else');
